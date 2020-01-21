7f406476a4e06038a6f346ddccd262046020635c
Glenna Xie <glennax@amazon.com>
2020-01-21 14:59:52 -0800

    component design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card>
+      <AnimationIndicator />
+      <Image />
+      <Stats />
+    </Card>
   );
+}
+
+// Card
+class Card extends React.Component {
+  render() {
+    return <div className="card"></div>;
+  }
+}
+
+//Animation Indicator
+class AnimationIndicator extends React.Component {
+  render() {
+    return <div className="gif-target">[GIF]</div>;
+  }
+}
+
+//Image
+class Image extends React.Component {
+  render() {
+    return <div className="preview"></div>;
+  }
+}
+//Stats
+class Stats extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer-item">views</div>
+        <div className="footer-item">likes</div>
+        <div className="footer-item">comments</div>
+      </div>
+    );
+  }
+}
+
+//Details
+class Details extends React.Component {
+  render() {
+    return (
+      <div className="desc">
+        <p id="title"></p>
+        <p id="description"></p>
+        <p id="created-at"></p>
+      </div>
+    );
+  }
 }
 
 export default App;
